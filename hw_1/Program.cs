76ba1fdde7e7652d982ee8ea51040c53da02e7fb
PavelSheglov <tpaul-1981@yandex.ru>
2018-05-24 13:05:20 +0700

    Домашнее задание к лекции №6. Задача на запись-чтение чисел в/из файла. Задача на работу с XML документом

--- hw_1/Program.cs
+++ hw_1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace hw_1
 {
@@ -10,6 +11,92 @@ namespace hw_1
     {
         static void Main(string[] args)
         {
+            var numbers = InputData();
+            string file = @"..\..\Files\numbers.txt";
+            Console.WriteLine("Нажмите любую клавишу для записи данных в файл.....");
+            Console.ReadKey();
+            if (WriteData(numbers, file))
+                Console.WriteLine("Данные успешно записаны в файл {0}", file);
+            else
+                Console.WriteLine("При записи в файл {0} произошла ошибка!!!", file);
+            Console.WriteLine("Нажмите любую клавишу для чтения данных из файла.....");
+            Console.ReadKey();
+            var loadNumbers = ReadData(file);
+            if (loadNumbers.Count > 0)
+            {
+                Console.WriteLine("Данные успешно считаны из файла {0}", file);
+                foreach (var number in loadNumbers)
+                    Console.WriteLine(number);
+            }
+            else
+                Console.WriteLine("При чтении из файла {0} произошла ошибка!!!", file);
+        }
+
+        public static List<short> InputData()
+        {
+            bool isFinal = false;
+            List<short> numbers = new List<short>();
+            while (!isFinal)
+            {
+                bool isOk = false;
+                short number = 0;
+                Console.Clear();
+                while (!isOk)
+                {
+                    try
+                    {
+                        Console.WriteLine("Введите целое число (от {0} до {1}):", short.MinValue, short.MaxValue);
+                        number = Convert.ToInt16(Console.ReadLine());
+                        isOk = true;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Значение не является целым числом. Повторите ввод....");
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine("Значение вне допустимого диапазона. Повторите ввод....");
+                    }
+                }
+                numbers.Add(number);
+                Console.WriteLine("Продолжить ввод?\n1.Да\n2.Нет");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        isFinal = false;
+                        break;
+                    case "2":
+                    default:
+                        isFinal = true;
+                        break;
+                }
+            }
+            return numbers;
+        }
+        public static bool WriteData(List<short> numbers, string fileName)
+        {
+            bool result = false;
+            using (var stream = new StreamWriter(fileName))
+            {
+                foreach (var number in numbers)
+                    stream.WriteLine(number.ToString());
+                result = true;
+            }
+            return result;
+        }
+        public static List<short> ReadData(string fileName)
+        {
+            var result = new List<short>();
+            using (var stream = new StreamReader(fileName))
+            {
+                string number = stream.ReadLine();
+                while(number!=null)
+                {
+                    result.Add(Convert.ToInt16(number));
+                    number = stream.ReadLine();
+                }
+            }
+            return result;
         }
     }
 }
---
+++ hw_2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
+
+namespace hw_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathXML = @"..\..\Files\contacts.xml";
+            string pathPhones= @"..\..\Files\phones.txt";
+            string pathEmails = @"..\..\Files\emails.txt";
+            var documentXML = LoadXMLDocument(pathXML);
+            Console.WriteLine("Исходный XML документ {0}:", pathXML);
+            if (documentXML != null)
+            {
+                Console.WriteLine(documentXML);
+                Console.WriteLine("Выгрузка списка телефонов в файл {0}", pathPhones);
+                if (WriteList(GetListOfPhones(documentXML), pathPhones))
+                    Console.WriteLine("Выгрузка завершена");
+                else
+                    Console.WriteLine("При выгрузке в файл произошла ошибка");
+                Console.WriteLine("Выгрузка списка почтовых адресов в файл {0}", pathEmails);
+                if (WriteList(GetListOfEmails(documentXML), pathEmails))
+                    Console.WriteLine("Выгрузка завершена");
+                else
+                    Console.WriteLine("При выгрузке в файл произошла ошибка");
+            }
+            else
+                Console.WriteLine("При чтении файла произошла ошибка или он пустой");
+        }
+
+        static XDocument LoadXMLDocument(string fileName)
+        {
+            XDocument document = null;
+            using (var resource = new StreamReader(fileName))
+            {
+                document = XDocument.Load(resource);
+            }
+            return document;
+        }
+
+        static List<string> GetListOfPhones(XDocument document)
+        {
+            var phones = new List<string>();
+            if(document!=null)
+            {
+                List<XElement> xcontacts = document.Root.Elements("Contact").ToList();
+                List<XElement> xphones = xcontacts.FindAll(element => element.Attribute("Type").Value == "Phone");
+                foreach (var phone in xphones)
+                    phones.Add(phone.Attribute("Name").Value.ToString() + " " + phone.Value.ToString());
+            }
+            return phones;
+        }
+
+        static List<string> GetListOfEmails(XDocument document)
+        {
+            var emails = new List<string>();
+            if (document != null)
+            {
+                List<XElement> xcontacts = document.Root.Elements("Contact").ToList();
+                List<XElement> xemails = xcontacts.FindAll(element => element.Attribute("Type").Value == "Email");
+                foreach (var email in xemails)
+                    emails.Add(email.Attribute("Name").Value.ToString() + " " + email.Value.ToString());
+            }
+            return emails;
+        }
+
+        static bool WriteList(List<string> records, string fileName)
+        {
+            bool result = false;
+            using (var stream = new StreamWriter(fileName))
+            {
+                foreach (var record in records)
+                    stream.WriteLine(record.ToString());
+                result = true;
+            }
+            return result;
+        }
+    }
+}
